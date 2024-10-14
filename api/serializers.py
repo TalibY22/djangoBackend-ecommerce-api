7cef288c97b9eb87da177d76a0325def7f9b705c
TalibY22 <hzhzhhsj58@gmail.com>
2024-10-14 11:18:47 +0300

    petty_additions

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,21 @@
-from . import models
+from .models import customers,products
 from rest_framework import serializers
 from django.contrib.auth.models import Group, User
 
 
+class CustomersSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = customers
+        fields = ['first_name', 'last_name', 'email', 'phone_number','address','dob']
 
 
+class ProductsSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = products
+        fields='__all__'
+
+#should return customer_details
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
--- api/views.py
+++ api/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render,get_object_or_404
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework import status
+from .models import customers
+from rest_framework.authtoken.models import Token
+from  .serializers import LoginSerializer,CustomersSerializer,ProductsSerializer
+from django.contrib.auth import authenticate
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
+
+
+@api_view(['POST'])
+def login_api(request):
+    serializer = LoginSerializer(data=request.data)
+    if serializer.is_valid():
+        username = serializer.validated_data['username']
+        password = serializer.validated_data['password']
+        user = authenticate(username=username, password=password)
+        if user:
+            token, created = Token.objects.get_or_create(user=user)
+            customer = customer.objects.get(user=user)
+            customer_Serializer = CustomersSerializer(customer)
+            return Response({'token': token.key})
+        else:
+            return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+    else:
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
