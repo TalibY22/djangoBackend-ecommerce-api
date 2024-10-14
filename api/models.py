48200e51fa11d44f3ce8959202ec726b8fa8ef4e
TalibY22 <hzhzhhsj58@gmail.com>
2024-10-14 10:05:10 +0300

    database-construction_in-progress

--- api/models.py
+++ api/models.py
@@ -1,3 +1,69 @@
 from django.db import models
+from typing import Any, Iterable
+from django.contrib.auth.models import User
+from django.contrib.auth import get_user
+from datetime import timedelta
 
 # Create your models here.
+
+class customers(models.Model):
+     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
+     first_name  = models.CharField(max_length=20)
+     last_name = models.CharField(max_length=20)
+     email = models.EmailField()
+     phone_number = models.IntegerField()
+     address = models.CharField()
+     dob = models.DateField()
+
+     #Create a user for the customers 
+     def save(self, *args, **kwargs):
+        if not self.user:
+           
+            username = self.First_Name.lower()
+            password = "123456789"  
+
+            
+            self.user = User.objects.create_user(username=username, password=password)
+        
+        
+        super().save(*args, **kwargs)
+
+
+class category(models.Model):
+    category_type = models.CharField(max_length=20)
+
+
+
+class products(models.Model):
+
+    name = models.CharField(max_length=200)
+    price = models.IntegerField()
+    descrption = models.CharField(max_length=200)
+    summary = models.CharField(max_length=200)
+    SKU = models.IntegerField()
+    
+    category_type = models.ForeignKey(category, on_delete=models.CASCADE)
+
+
+class wishlist(models.Model):
+      customer_id = models.ForeignKey(customers, on_delete=models.CASCADE)
+      product_id = models.ForeignKey(products, on_delete=models.CASCADE)
+
+
+class cart(models.Model):
+      customer_id = models.ForeignKey(customers, on_delete=models.CASCADE)
+      total = models.IntegerField()
+
+
+class cart_item(models.Model):
+       cart_id = models.ForeignKey(cart, on_delete=models.CASCADE)
+       product_id = models.ForeignKey(products, on_delete=models.CASCADE)
+       quantity = models.IntegerField()
+
+
+
+
+
+
+
+    
